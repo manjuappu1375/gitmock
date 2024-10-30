9b80c2f519cb512e7c5f73f0b61d781587fc8dd8
manjunath <manjuappu1375@gmail.com>
2024-10-30 15:04:12 +0530

    updated Test2.java in branch2

--- Test2.java
+++ Test2.java
@@ -2,6 +2,6 @@ class Test2
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program.git and github mockx");
+        System.out.println("My First Java Program github mock");
     }
 };
